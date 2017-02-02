6a09d04395b5885cdfd37413de614445ab27bbbd
RafiBomb <rafi@zurb.com>
2017-02-01 22:30:43 -0800

    removes hollow class in favor of variable setting and adds animation on hover

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,3 +5,7 @@ var mySVGsToInject = document.querySelectorAll('.inject-me');
 
 // Do the SVG injection
 SVGInjector(mySVGsToInject);
+
+$('[data-fadeInOut]').hover(function () {
+  $('.card-hover-reveal-section').toggleClass("fade-in");
+});
